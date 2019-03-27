e6390195f9b7d7992038517ee29a776cb0a7d3b0
BuiThanhHai25091998 <thmn2302@gmail.com>
2019-03-27 22:35:36 +0700

    thanh hai
    3333

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Slide;
 use App\Product;
+use App\ProductType;
+use App\BillDetail;
 
 use Illuminate\Http\Request;
 
@@ -17,14 +19,22 @@ class PageController extends Controller
     	return view('page.trangchu',compact('slide','new_product','product_promotion'));
     }
 
-    public function getLoaiSp()
+    public function getLoaiSp($type)
     {
-    	return view('page.loai_sanpham');
+        $sp_theoloai = Product::where('id_type',$type)->get();
+        $sp_khac = Product::where('id_type','<>',$type)->paginate(3);
+        $loai = ProductType::all();
+        $loai_sp = ProductType::where('id',$type)->first();
+    	return view('page.loai_sanpham',compact('sp_theoloai','sp_khac','loai','loai_sp'));
     }
 
-    public function getChiTiet()
+    public function getChiTiet(Request $req)
     {
-    	return view('page.chitiet_sanpham');
+        $sanpham = Product::where('id',$req->id)->first();
+        $sp_cungloai = Product::where('id_type',$sanpham->id_type)->paginate(3);
+        $sp_moi = Product::where('new',1)->paginate(4);
+        $sp_banchay = BillDetail::where('quatity','<>',1)->get();
+    	return view('page.chitiet_sanpham',compact('sanpham','sp_cungloai','sp_moi','sp_banchay'));
     }
 
     public function getLienHe()
--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -46,7 +46,7 @@
 										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
 									@endif
 										<div class="single-item-header">
-											<a href="product.html"><img src="source/image/product/{{$newPrd->image}}" alt="" height="250px"></a>
+											<a href="{{route('chitietsanpham',$newPrd->id)}}"><img src="source/image/product/{{$newPrd->image}}" alt="" height="250px"></a>
 										</div>
 										<div class="single-item-body">
 											<p class="single-item-title">{{$newPrd->name}}</p>
@@ -61,7 +61,7 @@
 										</div>
 										<div class="single-item-caption">
 											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<a class="beta-btn primary" href="{{route('chitietsanpham',$newPrd->id)}}">Details <i class="fa fa-chevron-right"></i></a>
 											<div class="clearfix"></div>
 										</div>
 									</div>
@@ -86,7 +86,7 @@
 										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
 
 										<div class="single-item-header">
-											<a href="product.html"><img src="source/image/product/{{$prdp->image}}" alt="" height="250px"></a>
+											<a href="{{route('chitietsanpham',$prdp->id)}}"><img src="source/image/product/{{$prdp->image}}" alt="" height="250px"></a>
 										</div>
 										<div class="single-item-body">
 											<p class="single-item-title">{{$prdp->name}}</p>
@@ -97,7 +97,7 @@
 										</div>
 										<div class="single-item-caption">
 											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<a class="beta-btn primary" href="{{route('chitietsanpham',$prdp->id)}}">Details <i class="fa fa-chevron-right"></i></a>
 											<div class="clearfix"></div>
 										</div>
 									</div>
